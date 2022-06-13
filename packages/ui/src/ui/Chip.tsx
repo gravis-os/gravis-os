c8cbd1e6d4eeee366f72f2b5b2f67a548f0b44a3
Alex <alex@onextech.com>
2022-06-13 19:02:24 +0800

    feat: update chip to allow custom colors

--- packages/ui/src/ui/Chip.tsx
+++ packages/ui/src/ui/Chip.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import { Chip as MuiChip, ChipProps as MuiChipProps } from '@mui/material'
 
-export interface ChipProps extends MuiChipProps {}
+// #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla(), color()
+const COLOR_REGEX =
+  /#[a-z0-9]{3}|#[a-z0-9]{6}|rgba?\(.+\)|hsla?\(.+\)|color\(.+\)/i
 
-const Chip: React.FC<ChipProps> = props => {
-  return <MuiChip {...props} />
+export interface ChipProps extends Omit<MuiChipProps, 'color'> {
+  title?: string
+  color?: string
+}
+
+const Chip: React.FC<ChipProps> = (props) => {
+  const { title, color, ...rest } = props
+  const isCustomColor = color && COLOR_REGEX.test(color)
+  const customColorSx = isCustomColor && {
+    '&&': {
+      backgroundColor: color,
+      color: (theme) => theme.palette.getContrastText(color),
+    },
+  }
+  return (
+    <MuiChip
+      label={title}
+      color={isCustomColor ? undefined : (color as MuiChipProps['color'])}
+      {...rest}
+      sx={{
+        ...customColorSx,
+        ...rest.sx,
+      }}
+    />
+  )
 }
 
 export default Chip
