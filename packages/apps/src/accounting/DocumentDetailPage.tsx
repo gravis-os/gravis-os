306cd5b12180f46c45dfee6e0d4c868da8461b55
Alex <alex@onextech.com>
2022-06-15 22:12:25 +0800

    fix: fix relations not saving in crud forms

--- packages/apps/src/accounting/DocumentDetailPage.tsx
+++ packages/apps/src/accounting/DocumentDetailPage.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Button } from '@gravis-os/ui'
 import { DetailPage, DetailPageProps } from '@gravis-os/crud'
 import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined'
-import omit from 'lodash/omit'
 import { useRouter } from 'next/router'
 import getQuotationTitle from './getQuotationTitle'
 import DocumentFormSections from './DocumentFormSections'
@@ -38,11 +37,7 @@ const DocumentDetailPage: React.FC<DocumentDetailPageProps> = (props) => {
           afterDelete: () => router.push(module.route.plural),
           setFormValues: (props) => {
             const { values, isNew } = props
-            // TODO@Alex: Save lines properly into DB
-
-            // Temp fix to avoid network error: "column _.lines does not exist"
-            // TODO@Alex: Instead of omitting, get the useCrudForm to save to the relation automatically to DB based on quotation_line
-            return omit(values, ['lines'])
+            return values
           },
           defaultValues: { title: getQuotationTitle() },
         },
