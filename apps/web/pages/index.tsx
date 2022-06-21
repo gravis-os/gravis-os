875ccedb825d6aa11fae11d6e1e0bfe086fbd467
clodal <joel@onextech.com>
2022-06-21 23:08:17 +0800

    feat(web): add next-seo

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 import * as React from 'react'
 import Container from '@mui/material/Container'
 import Button from '@mui/material/Button'
@@ -8,53 +9,60 @@ import ProTip from '../src/ProTip'
 import Copyright from '../src/Copyright'
 
 const Home: NextPage = () => (
-  <Container maxWidth="sm">
-    <Box sx={{ my: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        MUI v5 + Next.js with TypeScript example 1
-      </Typography>
+  <>
+    <NextSeo
+      title="Simple Home Example"
+      description="A short description goes here."
+    />
 
-      <Stack spacing={1}>
-        <Button href="/about" color="primary">
-          About page
-        </Button>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          MUI v5 + Next.js with TypeScript example 1
+        </Typography>
 
-        <Button href="/posts" color="primary">
-          Posts page
-        </Button>
+        <Stack spacing={1}>
+          <Button href="/about" color="primary">
+            About page
+          </Button>
 
-        <Button href="/contact" color="primary">
-          Contact page
-        </Button>
+          <Button href="/posts" color="primary">
+            Posts page
+          </Button>
 
-        <Button href="/profile" color="primary">
-          Profile page
-        </Button>
+          <Button href="/contact" color="primary">
+            Contact page
+          </Button>
 
-        <Button href="/auth" color="primary">
-          Auth page
-        </Button>
-      </Stack>
+          <Button href="/profile" color="primary">
+            Profile page
+          </Button>
 
-      <ProTip />
-      <Copyright />
-    </Box>
+          <Button href="/auth" color="primary">
+            Auth page
+          </Button>
+        </Stack>
 
-    <Alert severity="info" sx={{ py: 3 }}>
-      Hello World
-    </Alert>
+        <ProTip />
+        <Copyright />
+      </Box>
 
-    <Box
-      sx={{
-        py: 3,
-        backgroundColor: 'primary.main',
-        color: 'primary.contrastText',
-      }}
-      center
-    >
-      Hello World {printNumber(1000000, { type: 'amount', dp: 2 })}
-    </Box>
-  </Container>
+      <Alert severity="info" sx={{ py: 3 }}>
+        Hello World
+      </Alert>
+
+      <Box
+        sx={{
+          py: 3,
+          backgroundColor: 'primary.main',
+          color: 'primary.contrastText',
+        }}
+        center
+      >
+        Hello World {printNumber(1000000, { type: 'amount', dp: 2 })}
+      </Box>
+    </Container>
+  </>
 )
 
 export default Home
