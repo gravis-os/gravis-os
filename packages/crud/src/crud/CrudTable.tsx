ec7dd65af7a70f6b4b46ec56b8ad577861560963
Alex <alex@onextech.com>
2022-07-12 22:09:17 +0800

    fix(crud): fix CrudTableColumn type

--- packages/crud/src/crud/CrudTable.tsx
+++ packages/crud/src/crud/CrudTable.tsx
@@ -15,7 +15,7 @@ import CrudPreviewDrawer from './CrudPreviewDrawer'
 import fetchCrudItems from './fetchCrudItems'
 
 type CrudTableColumn =
-  | (ColDef & { hide: boolean | (({ user }) => boolean) })
+  | (ColDef & { hide?: boolean | (({ user }) => boolean) })
   | (ColGroupDef & {
       hasAvatar?: boolean
     })
