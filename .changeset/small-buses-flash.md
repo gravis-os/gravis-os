38b6ff2f4e9fa7d1a6c3145dbcf087d151f41425
clodal <joel@onextech.com>
2022-08-23 23:21:58 +0800

    feat(ui): add SettingsProvider to manage theming and other user preferences in future

---
+++ .changeset/small-buses-flash.md
@@ -0,0 +1,5 @@
+---
+"@gravis-os/ui": patch
+---
+
+Add SettingsProvider to manage user theming and more in future
---
+++ packages/ui/src/ui/SettingsProvider.tsx
@@ -0,0 +1,114 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+export interface Settings {
+  direction?: 'ltr' | 'rtl'
+  responsiveFontSizes?: boolean
+  theme: 'light' | 'dark'
+}
+
+export interface SettingsContextValue {
+  settings: Settings
+  saveSettings: (update: Settings) => void
+  isDarkMode: boolean
+}
+
+export interface SettingsProviderProps {
+  children?: React.ReactNode
+}
+
+const initialSettings: Settings = {
+  direction: 'ltr',
+  responsiveFontSizes: true,
+  theme: 'light',
+}
+
+const restoreSettings = (): Settings | null => {
+  let settings: any = null
+
+  try {
+    const storedData: string | null =
+      globalThis.localStorage.getItem('settings')
+
+    if (storedData) {
+      settings = JSON.parse(storedData)
+    } else {
+      settings = {
+        direction: 'ltr',
+        responsiveFontSizes: true,
+        theme: globalThis.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light',
+      }
+    }
+  } catch (err) {
+    console.error(err)
+    // If stored data is not a strigified JSON this will fail,
+    // that's why we catch the error
+  }
+
+  return settings
+}
+
+const storeSettings = (settings: Settings): void => {
+  globalThis.localStorage.setItem('settings', JSON.stringify(settings))
+}
+
+export const SettingsContext = createContext<SettingsContextValue>({
+  settings: initialSettings,
+  saveSettings: () => null,
+  isDarkMode: false,
+})
+
+export const SettingsConsumer = SettingsContext.Consumer
+
+export const useSettings = () => {
+  return React.useContext(SettingsContext)
+}
+
+/**
+ * SettingsProvider
+ * @param props
+ * @constructor
+ * @example
+ * <SettingsProvider>
+ *   <SettingsConsumer>
+ *     {({ settings }) => (
+ *       <ThemeProvider theme={getAdminTheme(settings.theme)}>
+ *         <CssBaseline />
+ *           <Component {...pageProps} />
+ *       </ThemeProvider>
+ *     )}
+ *   </SettingsConsumer>
+ * </SettingsProvider>
+ */
+const SettingsProvider: React.FC<SettingsProviderProps> = (props) => {
+  const { children } = props
+  const [settings, setSettings] = useState<Settings>(initialSettings)
+
+  useEffect(() => {
+    const restoredSettings = restoreSettings()
+
+    if (restoredSettings) {
+      setSettings(restoredSettings)
+    }
+  }, [])
+
+  const saveSettings = (updatedSettings: Settings): void => {
+    setSettings(updatedSettings)
+    storeSettings(updatedSettings)
+  }
+
+  return (
+    <SettingsContext.Provider
+      value={{
+        settings,
+        saveSettings,
+        isDarkMode: settings.theme === 'dark',
+      }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  )
+}
+
+export default SettingsProvider
