f4fa6024da1085d61af7930872f637192ad43c46
Roby <roby@onextech.com>
2022-09-16 11:11:10 +0800

    refactor(ui): rename mobile UI components

--- packages/ui/src/ui/Mobile/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as MobileCardIconButtonRow } from './MobileCardIconButtonRow'
-export * from './MobileCardIconButtonRow'
-
-export { default as MobileListCard } from './MobileListCard'
-export * from './MobileListCard'
-
-export { default as VerticalIconButton } from './VerticalIconButton'
-export * from './VerticalIconButton'
