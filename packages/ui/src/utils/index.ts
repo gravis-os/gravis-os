a7e3c77c3fbd6b9e763a58875209d24ac05708c5
Roby <roby@onextech.com>
2022-09-16 11:12:08 +0800

    fix(ui): add renderReactNodeOrString to ui exports

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,2 +1,5 @@
 export { default as getResponsiveSxProp } from './getResponsiveSxProp'
 export * from './getResponsiveSxProp'
+
+export { default as renderReactNodeOrString } from './renderReactNodeOrString'
+export * from './renderReactNodeOrString'
