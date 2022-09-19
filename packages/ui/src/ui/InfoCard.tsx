5025fe2915d3b39870e16b691ab1b1ca40f136ce
Roby <roby@onextech.com>
2022-09-19 17:20:34 +0800

    feat(ui): add href to InfoCard

--- packages/ui/src/ui/InfoCard.tsx
+++ packages/ui/src/ui/InfoCard.tsx
@@ -4,23 +4,53 @@ import Card, { CardProps } from './Card'
 import Chip from './Chip'
 import Stack from './Stack'
 import Typography, { TypographyProps } from './Typography'
+import withHref from './withHref'
 
+/**
+ * Property of the structured InfoCard content section.
+ *
+ * @prop {ReactNode} title
+ * @prop {ReactNode} description
+ */
 export interface InfoCardItemProps {
+  /** Renders a prestyled Typography if string is given, else it will render the given ReactNode */
   title: ReactNode
+  /** Renders a prestyled Typography if string is given, else it will render the given ReactNode */
   description?: ReactNode
 }
 
+/**
+ * Property of the InfoCard.
+ *
+ * @extends {CardProps}
+ * @prop {string} key
+ * @prop {ReactNode} title
+ * @prop {InfoCardItemProps[]} items
+ * @prop {TypographyProps} titleTypographyProps?
+ * @prop {ReactNode} chip?
+ * @prop {ReactNode} icon?
+ * @prop {string} href?
+ */
 export interface InfoCardProps extends CardProps {
   key: string
   title: ReactNode
-  titleTypographyProps?: TypographyProps
-  chip?: ReactNode
-  icon?: ReactNode
+  /** Prestructured card content reprented by items */
   items: InfoCardItemProps[]
+  titleTypographyProps?: TypographyProps
+  /**
+   * If string is given, it will render in a preset styled chip.
+   * Else, whole React node will be rendered on the right side of the Card header
+   */
+  chip?: ReactNode
+  /** Icons displayed to the left of the title */
+  icon?: ReactNode
+  /** URL destination when the InfoCard is clicked */
+  href?: string
 }
 
 const InfoCard: React.FC<InfoCardProps> = (props): React.ReactElement => {
-  const { key, title, titleTypographyProps, chip, icon, items, ...rest } = props
+  const { key, title, titleTypographyProps, chip, icon, items, href, ...rest } =
+    props
 
   /* Card Header */
   const structuredTitle = isValidElement(title) ? (
@@ -46,24 +76,28 @@ const InfoCard: React.FC<InfoCardProps> = (props): React.ReactElement => {
       )}
     </Stack>
   )
-
-  return (
-    <>
-      <Card key={key} title={structuredTitle} {...rest}>
-        <Stack spacing={2}>
-          {items.map((item) => {
-            const { title, description } = item
-            return (
-              <Stack spacing={1}>
-                {renderReactNodeOrString(title, { variant: 'subtitle2' })}
-                {renderReactNodeOrString(description, { variant: 'body2' })}
-              </Stack>
-            )
-          })}
-        </Stack>
-      </Card>
-    </>
+  const cardProps: CardProps = {
+    key,
+    title: structuredTitle,
+    ...(href && { sx: { cursor: 'pointer' } }),
+  }
+  const childrenJsx = (
+    <Card {...cardProps} {...rest}>
+      <Stack spacing={2}>
+        {items.map((item) => {
+          const { title, description } = item
+          return (
+            <Stack spacing={1}>
+              {renderReactNodeOrString(title, { variant: 'subtitle2' })}
+              {renderReactNodeOrString(description, { variant: 'body2' })}
+            </Stack>
+          )
+        })}
+      </Stack>
+    </Card>
   )
+
+  return <>{withHref({ href })(childrenJsx)}</>
 }
 
 export default InfoCard
