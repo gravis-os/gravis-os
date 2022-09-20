4d4a74016b70b7ee7c69601a36f632aa40940ed0
fernando <fernando@onextech.com>
2022-09-20 14:47:51 +0800

    feat(crud): add modelFieldProps to ModelFieldWithCrud

--- packages/crud/src/crud/ModelFieldWithCrud.tsx
+++ packages/crud/src/crud/ModelFieldWithCrud.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormSectionProps } from '@gravis-os/form'
+import { FormSectionProps, ModelFieldProps } from '@gravis-os/form'
 import { CrudModule } from '@gravis-os/types'
 import useAddDialog from './useAddDialog'
 import CrudAddDialog, { CrudAddDialogProps } from './CrudAddDialog'
@@ -9,10 +9,17 @@ export interface RenderModelFieldWithCrudProps {
   crudAddDialogProps?: CrudAddDialogProps
   module: CrudModule
   addFormSections: FormSectionProps[]
+  modelFieldProps?: ModelFieldProps
 }
 
 const ModelFieldWithCrud: React.FC<RenderModelFieldWithCrudProps> = (props) => {
-  const { children, module, addFormSections, crudAddDialogProps } = props
+  const {
+    children,
+    module,
+    addFormSections,
+    crudAddDialogProps,
+    modelFieldProps,
+  } = props
   const useAddDialogProps = useAddDialog({
     module,
     addFormSections,
@@ -23,6 +30,7 @@ const ModelFieldWithCrud: React.FC<RenderModelFieldWithCrudProps> = (props) => {
     <>
       {/* ModelField */}
       {React.cloneElement(children, {
+        ...modelFieldProps,
         onCreateClick: () => setAddDialogOpen(true),
       })}
 
