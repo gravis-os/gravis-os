62348efe007556b9cf55c339b752e3d6b60c2d63
Steve <steve@onextech.com>
2022-09-23 17:22:14 +0800

    fix(rtl): fix ES modules imports, add polyfill TextEncoder, add required ENVs

--- packages/rtl/jest.config.js
+++ packages/rtl/jest.config.js
@@ -1,3 +1,5 @@
+const esModules = ['jose', '@supabase'].join('|')
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
@@ -5,4 +7,6 @@ module.exports = {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [`../../node_modules/(?!${esModules})`],
+  setupFilesAfterEnv: ['../../node_modules/@gravis-os/rtl/setup-jest.js'],
 }
