426b106a5a722f03bdb2fe87fd9118d83a2c20aa
fernando <fernando@onextech.com>
2022-09-26 09:46:40 +0800

    feat(web): add gravis theme landing

--- packages/landing/src/themes/Landing/gravisLandingTheme.ts
+++ packages/landing/src/themes/Landing/gravisLandingTheme.ts
@@ -1,4 +1,10 @@
 import merge from 'lodash/merge'
+import {
+  Palette,
+  PaletteMode,
+  PaletteOptions,
+  ThemeOptions,
+} from '@mui/material'
 import { baseTheme } from '@gravis-os/ui'
 import landingTheme from './landingTheme'
 
@@ -7,26 +13,62 @@ const headerFontFamily =
 const subtitleFontFamily =
   "SF Pro Text,-apple-system, Roboto, 'Helvetica Neue', Arial, sans-serif"
 
-const gravisLandingThemeConfig = {
-  typography: {
-    h1: { fontFamily: headerFontFamily },
-    h2: {
-      fontFamily: headerFontFamily,
-      lineHeight: 1.15555556,
-      letterSpacing: '-0.025em',
-      [baseTheme.light.breakpoints.up('lg')]: {
-        fontSize: baseTheme.light.typography.pxToRem(48),
+const textPrimaryLight = '#060A0E'
+
+type DeepPartial<T> = T extends object
+  ? { [P in keyof T]?: DeepPartial<T[P]> }
+  : T
+
+const getGravisLandingThemeConfig = (mode: PaletteMode): ThemeOptions => {
+  const isDarkMode = mode === 'dark'
+
+  const palette: DeepPartial<Palette> = {
+    primary: { main: '#fff' },
+    secondary: { main: '#59A5FE' },
+    text: {
+      primary: isDarkMode ? '#fff' : textPrimaryLight,
+      secondary: '#91929C',
+    },
+    background: {
+      default: isDarkMode ? '#1A1D25' : '#fff',
+    },
+  }
+
+  return {
+    typography: {
+      h1: { fontFamily: headerFontFamily },
+      h2: {
+        fontFamily: headerFontFamily,
+        lineHeight: 1.15555556,
+        letterSpacing: '-0.025em',
+        [baseTheme.light.breakpoints.up('lg')]: {
+          fontSize: baseTheme.light.typography.pxToRem(48),
+        },
+      },
+      h3: { fontFamily: headerFontFamily, fontWeight: 600 },
+      h4: { fontFamily: headerFontFamily },
+      h5: { fontFamily: subtitleFontFamily },
+    },
+    palette: palette as PaletteOptions,
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.color === 'primary' && {
+              color: textPrimaryLight,
+              '&:hover': { backgroundColor: '#fff' },
+            }),
+            textTransform: 'uppercase',
+          }),
+        },
       },
     },
-    h3: { fontFamily: headerFontFamily, fontWeight: 600 },
-    h4: { fontFamily: headerFontFamily },
-    h5: { fontFamily: subtitleFontFamily },
-  },
+  }
 }
 
 const gravisLandingTheme = {
-  light: merge({}, landingTheme.light, gravisLandingThemeConfig),
-  dark: merge({}, landingTheme.dark, gravisLandingThemeConfig),
+  light: merge({}, landingTheme.light, getGravisLandingThemeConfig('light')),
+  dark: merge({}, landingTheme.dark, getGravisLandingThemeConfig('dark')),
 }
 
 export default gravisLandingTheme
