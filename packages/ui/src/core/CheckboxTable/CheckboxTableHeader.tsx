186cd8ac17ed6af5e4a8328f13cbb110dc2887c7
winter-steve <steve@onextech.com>
2022-10-18 11:39:10 +0800

    fix(ui): fix react keys in CheckboxTableHeader

--- packages/ui/src/core/CheckboxTable/CheckboxTableHeader.tsx
+++ packages/ui/src/core/CheckboxTable/CheckboxTableHeader.tsx
@@ -19,16 +19,12 @@ const CheckboxTableHeader: React.FC<CheckboxTableHeaderProps> = (props) => {
       <Typography sx={{ fontWeight: 700, flexGrow: 1 }}>{title}</Typography>
       {hasToggleAllColumn && (
         <CheckboxTableColumn width={first(columnDefs).width}>
-          <Typography key="All" sx={{ textAlign: 'center' }}>
-            All
-          </Typography>
+          <Typography sx={{ textAlign: 'center' }}>All</Typography>
         </CheckboxTableColumn>
       )}
       {columnDefs.map((column) => (
-        <CheckboxTableColumn width={column.width}>
-          <Typography key={column.value} sx={{ textAlign: 'center' }}>
-            {column.label}
-          </Typography>
+        <CheckboxTableColumn key={column.value} width={column.width}>
+          <Typography sx={{ textAlign: 'center' }}>{column.label}</Typography>
         </CheckboxTableColumn>
       ))}
     </Stack>
