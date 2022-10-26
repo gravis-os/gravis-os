49fa7865e87915581acd36f77c56f2591a001e5f
fernando <fernando@onextech.com>
2022-10-26 16:22:50 +0800

    feat(auth, form): add fullscreen Auth Layout

--- packages/auth/src/auth/AuthLayout.tsx
+++ packages/auth/src/auth/AuthLayout.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import NextLink from 'next/link'
+import { StaticImageData } from 'next/image'
 import {
   Box,
   BoxProps,
@@ -7,31 +8,121 @@ import {
   Card,
   Container,
   Divider,
+  Grid,
+  Image,
   Stack,
+  Typography,
 } from '@gravis-os/ui'
 
 export interface AuthLayoutProps extends BoxProps {
   actions?: React.ReactElement
   children?: React.ReactElement
   disableResetPasswordButton?: boolean
-  title: string
   authRoutes?: {
     resetPassword?: string
   }
   logo?: React.ReactElement
+  logoProps?: BoxProps
+  fullScreen?: boolean
+  backgroundImgSrc?: string | StaticImageData
+  copyright?: string | ReactNode
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = (props) => {
   const {
     logo,
+    logoProps,
     actions,
     children,
     disableResetPasswordButton,
-    title,
     authRoutes,
     sx,
+    fullScreen,
+    backgroundImgSrc,
+    copyright,
     ...rest
   } = props
+
+  // JSX
+  const actionsJsx = (
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={1}
+      sx={{ width: 'auto' }}
+    >
+      {!disableResetPasswordButton && (
+        <Button href={authRoutes.resetPassword}>Reset Password</Button>
+      )}
+      {actions}
+    </Stack>
+  )
+  const copyrightJsx = (
+    <Typography variant="caption" sx={{ color: 'text.disabled' }}>
+      {copyright}
+    </Typography>
+  )
+  const logoJsx = (
+    <NextLink href="/" passHref>
+      <a>{logo}</a>
+    </NextLink>
+  )
+
+  if (fullScreen) {
+    return (
+      <Box component="main" {...rest} sx={sx}>
+        <Grid
+          container
+          spacing={0}
+          alignItems="stretch"
+          sx={{
+            backgroundColor: 'background.paper',
+            width: '100vw',
+            height: '100vh',
+          }}
+        >
+          <Grid item xs sx={{ height: '100%' }}>
+            <Stack
+              alignItems="center"
+              justifyContent="center"
+              sx={{
+                px: 5,
+                height: '100%',
+                textAlign: 'center',
+                position: 'relative',
+              }}
+            >
+              {logo && <Box {...logoProps}>{logoJsx}</Box>}
+              {children}
+              {actionsJsx}
+              {copyright && (
+                <Box
+                  width="100%"
+                  textAlign="center"
+                  position="absolute"
+                  bottom={0}
+                >
+                  {copyrightJsx}
+                </Box>
+              )}
+            </Stack>
+          </Grid>
+          {Boolean(backgroundImgSrc) && (
+            <Grid item xs={false} md={5} sx={{ height: '100%' }}>
+              <Image
+                src={backgroundImgSrc}
+                objectFit="cover"
+                containerSx={{
+                  height: '100%',
+                  pb: 0,
+                }}
+              />
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    )
+  }
 
   return (
     <Box
@@ -54,19 +145,19 @@ const AuthLayout: React.FC<AuthLayoutProps> = (props) => {
           },
         }}
       >
-        <Card elevation={16} sx={{ p: 4 }}>
+        <Card elevation={16} sx={{ p: 4, position: 'relative' }}>
           {logo && (
             <Box
+              {...logoProps}
               sx={{
                 alignItems: 'center',
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'center',
+                ...logoProps?.sx,
               }}
             >
-              <NextLink href="/" passHref>
-                <a>{logo}</a>
-              </NextLink>
+              {logoJsx}
             </Box>
           )}
           <Box
@@ -79,12 +170,12 @@ const AuthLayout: React.FC<AuthLayoutProps> = (props) => {
           </Box>
           <Divider sx={{ my: 3 }} />
 
-          <Stack direction="row" alignItems="center" spacing={1}>
-            {!disableResetPasswordButton && (
-              <Button href={authRoutes.resetPassword}>Reset Password</Button>
-            )}
-            {actions}
-          </Stack>
+          {actionsJsx}
+          {copyright && (
+            <Box width="100%" textAlign="center" mt={3}>
+              {copyrightJsx}
+            </Box>
+          )}
         </Card>
       </Container>
     </Box>
