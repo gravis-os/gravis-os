b15764842a40b217d03468290709e7187a823bf2
clodal <joel@onextech.com>
2022-11-16 23:20:10 +0800

    feat(ui): add responsive cols to ImageList

--- packages/ui/src/core/ImageList.tsx
+++ packages/ui/src/core/ImageList.tsx
@@ -3,24 +3,46 @@ import {
   ImageList as MuiImageList,
   ImageListProps as MuiImageListProps,
   ImageListItem as MuiImageListItem,
-  ImageListItemProps as MuiImageListItemProps,
+  Breakpoint,
 } from '@mui/material'
+import { ResponsiveSxProp } from '../utils'
 
-export interface ImageListProps extends Omit<MuiImageListProps, 'children'> {
+export interface ImageListProps
+  extends Omit<MuiImageListProps, 'children' | 'cols'> {
   items?: React.ReactNode[]
+  cols?: ResponsiveSxProp
 }
 
 const ImageList: React.FC<ImageListProps> = (props) => {
-  const { items, sx, ...rest } = props
+  const { items, cols = { xs: 1, md: 2 }, sx, ...rest } = props
 
   if (!items?.length) return null
 
   return (
     <MuiImageList
-      sx={{
-        mt: 0,
-        ...sx,
-      }}
+      sx={
+        {
+          mt: 0,
+
+          // Responsive cols
+          ...(typeof cols === 'object' && {
+            gridTemplateColumns: (theme) => {
+              return {
+                ...Object.entries(cols).reduce((acc, [key, value]) => {
+                  return {
+                    ...acc,
+                    [theme.breakpoints.up(key as Breakpoint)]: {
+                      gridTemplateColumns: `repeat(${value}, 1fr) !important`,
+                    },
+                  }
+                }, {}),
+              }
+            },
+          }),
+
+          ...sx,
+        } as MuiImageListProps['sx']
+      }
       {...rest}
     >
       {items.map((item) => {
