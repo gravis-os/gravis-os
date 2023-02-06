3722506e422457f0f5f9fb1bbbda3bd68edb4865
clodal <joel@onextech.com>
2023-02-06 13:35:35 +0800

    feat(utils): add getJsxFromReactNodeOrElement

---
+++ packages/utils/src/react/getJsxFromReactNodeOrElement.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export type ReactNodeOrElement =
+  | React.ReactNode
+  | React.JSXElementConstructor<unknown>
+
+/**
+ * Move to gravis
+ * @param item
+ */
+const getJsxFromReactNodeOrElement = (
+  item: ReactNodeOrElement,
+  itemProps = {}
+): React.ReactNode | undefined => {
+  const shouldRender = item && typeof item === 'object'
+  if (!shouldRender) return
+
+  const isReactNode = React.isValidElement(item)
+  if (isReactNode) return item
+
+  const Item = item as unknown as React.JSXElementConstructor<unknown>
+  return <Item {...itemProps} />
+}
+
+export default getJsxFromReactNodeOrElement
--- packages/utils/src/react/index.ts
+++ packages/utils/src/react/index.ts
@@ -1,2 +1,5 @@
+export { default as getJsxFromReactNodeOrElement } from './getJsxFromReactNodeOrElement'
+export * from './getJsxFromReactNodeOrElement'
+
 export { default as useStateWithCallback } from './useStateWithCallback'
 export * from './useStateWithCallback'
