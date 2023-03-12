9021ec5b2d65aa1d5b62d304a5aa8d222e93f695
clodal <joel@onextech.com>
2023-03-12 14:44:34 +0800

    fix(utils): Fix issue with absolute urls being distorted in the cleanHref util

---
+++ .changeset/new-cherries-flow.md
@@ -0,0 +1,5 @@
+---
+"@gravis-os/utils": patch
+---
+
+Fix issue with absolute urls being distorted in the cleanHref util
--- packages/utils/src/cleaners/cleanHref.ts
+++ packages/utils/src/cleaners/cleanHref.ts
@@ -1,4 +1,8 @@
 // Ensure that a href link only contains 1 forward slash at the beginning with regex
-const cleanHref = (href: string): string => href?.replace(/\/\/+/g, '/') || ''
+const cleanHref = (href: string): string => {
+  if (!href) return ''
+  if (href.startsWith('http')) return href
+  return href.replace(/\/\/+/g, '/') || ''
+}
 
 export default cleanHref
