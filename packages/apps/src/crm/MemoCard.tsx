a49ac3feb66d2a64ede4a925a0532930ed027200
Yadong Zhang <yadong@onextech.com>
2023-05-09 13:11:17 +0800

    fix(apps): removed empty space between card header and card body.

--- packages/apps/src/crm/MemoCard.tsx
+++ packages/apps/src/crm/MemoCard.tsx
@@ -1,13 +1,5 @@
 import React, { useState } from 'react'
-import {
-  Box,
-  Card,
-  CardHeader,
-  IconButton,
-  Stack,
-  Typography,
-  Button,
-} from '@gravis-os/ui'
+import { Box, Card, IconButton, Stack, Typography, Button } from '@gravis-os/ui'
 import Popover from '@mui/material/Popover'
 import { printDateTime, printHtml } from '@gravis-os/utils'
 import { MoreHorizOutlined } from '@mui/icons-material'
@@ -49,49 +41,6 @@ const MemoCard: React.FC<MemoCardProps> = (props) => {
         </Typography>
       }
     >
-      {isMutable && (
-        <CardHeader
-          action={
-            <>
-              <IconButton aria-label="settings" onClick={handleClick}>
-                <MoreHorizOutlined />
-              </IconButton>
-              <Popover
-                id="memo-card-popover"
-                open={Boolean(anchorEl)}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-              >
-                <Stack>
-                  <Button
-                    aria-label="Edit item"
-                    onClick={() => {
-                      setIsEditing(true)
-                      handleClose()
-                    }}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    aria-label="Delete item"
-                    onClick={async () => {
-                      await onDelete?.(item)
-                      handleClose()
-                    }}
-                    color="error"
-                  >
-                    Delete
-                  </Button>
-                </Stack>
-              </Popover>
-            </>
-          }
-        />
-      )}
       <Stack spacing={1}>
         {isEditing ? (
           <>
@@ -118,48 +67,89 @@ const MemoCard: React.FC<MemoCardProps> = (props) => {
             </Stack>
           </>
         ) : (
-          <>
-            {/* Header */}
-            <Stack
-              direction="row"
-              alignItems="center"
-              justifyContent="space-between"
-              spacing={1}
-            >
-              {/* Title */}
-              <div>
-                <Typography display="inline-block" variant="subtitle1">
-                  {title}
+          <Stack direction="row" justifyContent="space-between">
+            <Stack spacing={1}>
+              {/* Header */}
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                spacing={1}
+              >
+                {/* Title */}
+                <div>
+                  <Typography display="inline-block" variant="subtitle1">
+                    {title}
+                  </Typography>
+                  {user && (
+                    <>
+                      {' '}
+                      by{' '}
+                      <Typography
+                        display="inline-block"
+                        variant="body1"
+                        color="text.secondary"
+                      >
+                        {user.full_name || user.title}
+                      </Typography>
+                    </>
+                  )}
+                </div>
+
+                {/* Right */}
+                {actions && <Box sx={{ textAlign: 'right' }}>{actions}</Box>}
+              </Stack>
+
+              {content && (
+                <Typography variant="body1">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: printHtml(content.replaceAll('\n', '<br />')),
+                    }}
+                  />
                 </Typography>
-                {user && (
-                  <>
-                    {' '}
-                    by{' '}
-                    <Typography
-                      display="inline-block"
-                      variant="body1"
-                      color="text.secondary"
-                    >
-                      {user.full_name || user.title}
-                    </Typography>
-                  </>
-                )}
-              </div>
-
-              {/* Right */}
-              {actions && <Box sx={{ textAlign: 'right' }}>{actions}</Box>}
+              )}
             </Stack>
-
-            {content && (
-              <Typography variant="body1">
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: printHtml(content.replaceAll('\n', '<br />')),
+            {isMutable && (
+              <Box>
+                <IconButton aria-label="settings" onClick={handleClick}>
+                  <MoreHorizOutlined />
+                </IconButton>
+                <Popover
+                  id="memo-card-popover"
+                  open={Boolean(anchorEl)}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
                   }}
-                />
-              </Typography>
+                >
+                  <Stack>
+                    <Button
+                      aria-label="Edit item"
+                      onClick={() => {
+                        setIsEditing(true)
+                        handleClose()
+                      }}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      aria-label="Delete item"
+                      onClick={async () => {
+                        await onDelete?.(item)
+                        handleClose()
+                      }}
+                      color="error"
+                    >
+                      Delete
+                    </Button>
+                  </Stack>
+                </Popover>
+              </Box>
             )}
-          </>
+          </Stack>
         )}
       </Stack>
     </Card>
