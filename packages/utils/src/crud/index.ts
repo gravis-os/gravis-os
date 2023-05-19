3686a44a69c577c19603e2fd74f758fe9576f881
shank1xt <shashank.acharya@onextech.com>
2023-05-19 09:39:12 +0800

    feat: add func to index.ts

--- packages/utils/src/crud/index.ts
+++ packages/utils/src/crud/index.ts
@@ -7,6 +7,9 @@ export * from './getCategoryFromCrudItem'
 export { default as getCategoryWithItemsAndHref } from './getCategoryWithItemsAndHref'
 export * from './getCategoryWithItemsAndHref'
 
+export { default as getRelatedCrudItemsByCategoryId } from './getRelatedCrudItemsByCategoryId'
+export * from './getRelatedCrudItemsByCategoryId'
+
 export { default as getRelatedCrudItemsByTagTitle } from './getRelatedCrudItemsByTagTitle'
 export * from './getRelatedCrudItemsByTagTitle'
 
