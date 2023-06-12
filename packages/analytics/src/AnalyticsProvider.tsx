af82e3e8fdddbe3c05b87785ef80af007aa15676
shank1xt <shashank.acharya@onextech.com>
2023-06-12 16:20:09 +0800

    feat: add analytics provider

---
+++ packages/analytics/src/AnalyticsProvider.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useGtmPageViewOnRouteChange } from './GoogleTagManager'
+
+export interface AnalyticsProviderProps {
+  children?: React.ReactNode
+}
+
+const AnalyticsProvider: React.FC<AnalyticsProviderProps> = (props) => {
+  const { children } = props
+
+  // Set page view on route change
+  useGtmPageViewOnRouteChange()
+
+  return <>{children}</>
+}
+
+export default AnalyticsProvider
--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,1 +1,4 @@
 export * from './GoogleTagManager'
+
+export { default as AnalyticsProvider } from './AnalyticsProvider'
+export * from './AnalyticsProvider'
