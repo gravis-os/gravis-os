8c5add4807f400be2c156f61e3b811685fa2ce84
Dev <john.himawan@onextech.com>
2023-06-13 11:41:14 +0800

    Remove posts from irrelevant tags

--- packages/utils/src/crud/getRelatedCrudItemsByTagTitle.ts
+++ packages/utils/src/crud/getRelatedCrudItemsByTagTitle.ts
@@ -4,7 +4,7 @@ const getRelatedCrudItemsByTagTitle = (
   items: Array<CrudItem & { tags: CrudItem[] }>,
   tagTitle: string
 ): CrudItem[] | [] => {
-  if (!tagTitle) return items || []
+  if (!tagTitle) return []
   return items.filter(({ tags }) => {
     const tagTitles = tags.map(({ title }) => title)
     return tagTitles.includes(tagTitle)
