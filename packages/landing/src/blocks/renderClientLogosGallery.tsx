85ad5d1462f60ff560a49285c5cd78e5b7de523c
benzlui <benjamin.lui@onextech.com>
2023-07-05 16:50:45 +0800

    chore: resolve minor issues in renderClientLogosGallery

--- packages/landing/src/blocks/renderClientLogosGallery.tsx
+++ packages/landing/src/blocks/renderClientLogosGallery.tsx
@@ -2,27 +2,26 @@ import { ClientLogo } from '@gravis-os/types'
 import renderClientLogoCardBlockItem from './renderClientLogoCardBlockItem'
 
 interface renderClientLogosGalleryProps {
-  clientLogos: ClientLogo[]
-  clientBlockHeader: string
+  items?: ClientLogo[]
+  title: React.ReactNode
 }
 
-const commonBlockProps = { center: true, maxWidth: 'md' }
-
 const renderClientLogosGallery = (props: renderClientLogosGalleryProps) => {
-  const { clientLogos, clientBlockHeader } = props
+  const { items, title } = props
   return {
     key: 'gallery',
-    ...commonBlockProps,
+    center: true,
+    maxWidth: 'md',
     dark: true,
     items: [
-      { type: 'h4', title: clientBlockHeader },
+      { type: 'h4', title },
       {
         type: 'grid',
         sx: { mt: { xs: 5, md: 10 } },
         maxWidth: 'xl',
         gridProps: { spacing: 1 },
         gridItemProps: { xs: 6, md: 4 },
-        gridItems: clientLogos.map((clientLogo) => {
+        gridItems: items.map((clientLogo) => {
           const { avatar_src, avatar_alt, avatar_width, avatar_height } =
             clientLogo
           return renderClientLogoCardBlockItem({
