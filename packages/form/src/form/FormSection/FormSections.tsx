e725db630577aa5111dfa55894ecc9ec259a7e3b
Dao Ngoc Hieu <hieu.daongoc@onextech.com>
2023-07-12 17:21:57 +0800

    fix(form): add container props

--- packages/form/src/form/FormSection/FormSections.tsx
+++ packages/form/src/form/FormSection/FormSections.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Grid } from '@gravis-os/ui'
+import { Grid, GridProps } from '@gravis-os/ui'
 import FormSection, { FormSectionProps } from './FormSection'
 
 export interface FormSectionsProps extends Partial<FormSectionProps> {
   sections: FormSectionProps[]
+  containerProps: GridProps
 }
 
 const FormSections: React.FC<FormSectionsProps> = (props) => {
-  const { sections, gridProps, ...rest } = props
+  const { sections, containerProps, ...rest } = props
   if (!sections?.length) return null
 
   return (
     // @ts-ignore
-    <Grid container spacing={2} {...gridProps}>
+    <Grid container spacing={2} {...containerProps}>
       {sections.map((section) => (
         <FormSection key={section.key} {...section} {...rest} />
       ))}
