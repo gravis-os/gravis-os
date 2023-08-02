c00bbd3f49e13d479064403543c3b9435aff813e
Yadong Zhang <yadong@onextech.com>
2023-08-02 12:21:46 +0800

    feat(mail): supported to reply email.

--- packages/mail/src/services/Sendgrid.ts
+++ packages/mail/src/services/Sendgrid.ts
@@ -1,4 +1,4 @@
-import Sendgrid, { MailDataRequired } from '@sendgrid/mail'
+import Sendgrid, { type MailDataRequired } from '@sendgrid/mail'
 
 Sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -8,14 +8,18 @@ const CustomSendgrid = {
     from: string
     subject: string
     html: string
+    headers?: Record<string, string>
+    replyTo?: MailDataRequired['replyTo']
     text?: string
     attachments?: MailDataRequired['attachments']
   }) => {
-    const { from, to, subject, html, text, attachments } = props
+    const { from, to, subject, html, text, attachments, headers, replyTo } = props
 
     const onSend = await Sendgrid.send({
+      headers,
       from: from || process.env.SENDGRID_FROM_EMAIL,
       to,
+      replyTo,
       subject,
       html,
       text,
