512704d32486e1a809884b476720fc69fe53ed46
Yadong Zhang <yadong@onextech.com>
2023-08-30 17:54:53 +0800

    feat(crud): support to show user full_name instead of id in meta section.

--- packages/crud/src/crud/metaFormSection.tsx
+++ packages/crud/src/crud/metaFormSection.tsx
@@ -1,6 +1,53 @@
 import React from 'react'
 import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined'
 
+export const metaFormSectionWithFullName = {
+  key: 'meta',
+  title: 'Meta',
+  subtitle: 'Manage meta',
+  icon: <InventoryOutlinedIcon />,
+  fields: [
+    {
+      key: 'id',
+      name: 'id',
+      type: 'input',
+      disabled: true,
+    },
+    [
+      {
+        key: 'created_at',
+        name: 'created_at',
+        type: 'input',
+        disabled: true,
+      },
+      {
+        key: 'created_by_full_name',
+        name: 'created_by_full_name',
+        label: 'created_by',
+        type: 'input',
+        disabled: true,
+      },
+    ],
+    [
+      {
+        key: 'updated_at',
+        name: 'updated_at',
+        type: 'input',
+        disabled: true,
+      },
+      {
+        key: 'updated_by_full_name',
+        name: 'updated_by_full_name',
+        label: 'updated_by',
+        type: 'input',
+        disabled: true,
+      },
+    ],
+  ],
+  collapsible: true,
+  defaultCollapsed: true,
+}
+
 /**
  * metaFormSection
  * Form Section to be used in CrudForm for standard metadata
