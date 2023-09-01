6b7c81c423aa031c50ce3b15243aca24cc552b15
Yadong Zhang <yadong@onextech.com>
2023-09-01 18:29:32 +0800

    fix(crud): setup readonly section to display full name in meta section.

--- packages/crud/src/crud/metaFormSection.tsx
+++ packages/crud/src/crud/metaFormSection.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined'
+import { useQuery } from 'react-query'
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
+import { FormSectionReadOnlyStack } from '@gravis-os/form'
+import { Grid } from '@gravis-os/ui'
 
 export const metaFormSectionWithFullName = {
   key: 'meta',
@@ -46,6 +50,80 @@ export const metaFormSectionWithFullName = {
   ],
   collapsible: true,
   defaultCollapsed: true,
+}
+
+export const renderReadOnlySection = ({ label, item }, userModuleTableName = "user") => {
+  const { data: creator } = useQuery({
+    queryFn: async () => {
+      const { data } = await supabaseClient
+        .from(userModuleTableName)
+        .select('full_name')
+        // @ts-ignore
+        .match({ id: item?.created_by })
+        .single()
+
+      return data
+    },
+    // @ts-ignore
+    queryKey: [userModuleTableName],
+    // @ts-ignore
+    enabled: Boolean(item?.created_by)
+  })
+
+  const { data: updater } = useQuery({
+    queryFn: async () => {
+      const { data } = await supabaseClient
+        .from(userModuleTableName)
+        .select('full_name')
+        // @ts-ignore
+        .match({ id: item?.updated_by })
+        .single()
+
+      return data
+    },
+    // @ts-ignore
+    queryKey: [userModuleTableName],
+    // @ts-ignore
+    enabled: Boolean(item?.updated_by)
+  })
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <FormSectionReadOnlyStack
+          label="id"
+          title={item?.id}
+        />
+      </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FormSectionReadOnlyStack
+            label="created_at"
+            title={item?.created_at}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FormSectionReadOnlyStack
+            label="created_by"
+            title={creator?.full_name}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FormSectionReadOnlyStack
+            label="updated_at"
+            title={item?.updated_at}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FormSectionReadOnlyStack
+            label="updated_by"
+            title={updater?.full_name}
+          />
+        </Grid>
+    </Grid>
+  )
 }
 
 /**
