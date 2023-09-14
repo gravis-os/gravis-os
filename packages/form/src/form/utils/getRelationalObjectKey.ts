445aa2bf437d04f93559a747224d2a838f110474
Dao Ngoc Hieu <hieu.daongoc@onextech.com>
2023-09-14 11:19:30 +0800

    fix(form): add comments

--- packages/form/src/form/utils/getRelationalObjectKey.ts
+++ packages/form/src/form/utils/getRelationalObjectKey.ts
@@ -1,8 +1,15 @@
 import last from 'lodash/last'
 
 // Always returns the singular version of the key
-const getRelationalObjectKey = (key: string, keepPrefix: boolean = true) =>
-  keepPrefix
+// By default we want to keep the key prefix
+// e.g for *LinesFieldArray: lines.[rowIndex].product_id -> lines.[rowIndex].product
+// But for searching/filtering: we only want the exact key
+// e.g lines.order_form_line.order_form.sales_order.project_id -> product
+const getRelationalObjectKey = (
+  key: string,
+  shouldKeepPrefix: boolean = true
+) =>
+  shouldKeepPrefix
     ? key.split('_id')[0]
     : `${(last(key.split('.')) as string).split('_id')[0]}`
 
