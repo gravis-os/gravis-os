0e52ec59f1b524669a4d18de3dd0f9a84b0062ee
clodal <joel@onextech.com>
2023-09-26 19:04:52 +0800

    fix(utils): Add guard in cleanHref util

--- packages/utils/src/cleaners/cleanHref.ts
+++ packages/utils/src/cleaners/cleanHref.ts
@@ -1,6 +1,6 @@
 // Ensure that a href link only contains 1 forward slash at the beginning with regex
 const cleanHref = (href: string): string => {
-  if (!href) return ''
+  if (typeof href !== 'string') return ''
   if (href.startsWith('http')) return href
   return href.replaceAll(/\/\/+/g, '/') || ''
 }
